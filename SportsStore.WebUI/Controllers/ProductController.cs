f81fa2f310b431baa2775fe3586607ec6d38242b
Tim.Parkins <tim.parkins@multiling.com>
2013-10-31 17:11:30 -0600

    Page 218

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SportsStore.Domain.Abstract;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using SportsStore.Domain.Abstract;
-using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductsRepository productRepository) {
             this.repository = productRepository;
         }
 
-        public ActionResult List()
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products);
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products.Where(p => category == null || p.Category == category).OrderBy(p => p.ProductId).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ?
+                        repository.Products.Count() : 
+                        repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            
+            return View(model);
         }
     }
 }
--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using System;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Ninject;
-using SportsStore.Domain.Entities;
-using SportsStore.Domain.Abstract;
-using Moq;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -28,14 +24,7 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>{
-                new Product { Name = "Football", Price = 25},
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running Shoes", Price = 95}
-            }.AsQueryable());
-
-            ninjectKernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+            ninjectKernel.Bind<IProductsRepository>().To<EFProductRepository>();
         }
     }
 }
