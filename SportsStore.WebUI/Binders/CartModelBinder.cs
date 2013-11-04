7b55d8e08c114bf9786b0f9fa4961dff6822e93b
Tim Parkins <tim.parkins+git@gmail.com>
2013-11-03 17:48:01 -0700

    page 255

---
+++ SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Entities;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext) 
+        {
+            // get the Cart from the session
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            // create the cart if there wasn't one in the session dataa
+            if (cart == null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            // return the cart
+            return cart;
+        }
+    }
+}
--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -4,6 +4,7 @@ using SportsStore.Domain.Concrete;
 using System;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Configuration;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -25,6 +26,13 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             ninjectKernel.Bind<IProductsRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            ninjectKernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
     }
 }
